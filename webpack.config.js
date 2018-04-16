84f851df5cecf5f4e7891429a9b47f21909d8504
Ian Homer <ian.homer@gmail.com>
2018-04-16 22:17:24 +0100

    Dynamic reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main/js/index.js',
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './target/webapp'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      filename: 'src/main/webapp/index.html'
+    })
+  ],
   output: {
-    path: path.resolve(__dirname, 'target'),
+    path: path.resolve(__dirname, 'target/webapp'),
     filename: 'bundle.js'
   },
   module: {
